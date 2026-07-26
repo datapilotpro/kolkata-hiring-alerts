5446779222aa534bd425d1748f08517713a18824
datapilotpro <ayan.shee45@gmail.com>
2026-07-26 19:47:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+DESIGNATIONS = [
+    "MIS Executive",
+    "MIS Analyst",
+    "Reporting Analyst",
+    "Reporting Executive",
+    "Data Analyst",
+    "Data Operations Analyst",
+    "BI Analyst",
+    "Business Intelligence Analyst",
+    "Operations Analyst",
+    "Business Operations Analyst"
+]
+
+LOCATIONS = [
+    "Kolkata",
+    "Howrah",
+    "Salt Lake",
+    "New Town",
+    "Durgapur",
+    "Kharagpur",
+    "Purba Bardhaman",
+    "Paschim Bardhaman",
+    "Bardhaman",
+    "Burdwan",
+    "West Bengal"
+]
