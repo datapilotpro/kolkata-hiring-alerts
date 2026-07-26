70550f39cf9c0353d4cf695b7a657b54dec8fb19
datapilotpro <ayan.shee45@gmail.com>
2026-07-26 13:37:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Kolkata Hiring Alerts Started Successfully!")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = """✅ Kolkata Hiring Alerts Started Successfully!
+
+GitHub Actions is working correctly.
+
+Next step: LinkedIn Hiring Search 🚀
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Message sent successfully.")
